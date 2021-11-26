4ab080a4ea61f02053cc0cc6565dd92a90feedd9
tombill143 <tombill143@gmail.com>
2021-11-26 10:39:37 +0100

    Initial commit

---
+++ src/main/java/com/example/friendshipneverends/FriendshipNeverEndsApplication.java
@@ -0,0 +1,13 @@
+package com.example.friendshipneverends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipNeverEndsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FriendshipNeverEndsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/friendshipneverends/FriendshipNeverEndsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friendshipneverends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipNeverEndsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
