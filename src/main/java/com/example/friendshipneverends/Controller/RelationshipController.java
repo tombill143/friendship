363023599b5b7bac0ab42a8ceb91c4d32e73a81d
annkarinacaro <anax1410@stud.kea.dk>
2021-12-03 11:51:05 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/friendshipneverends/Controller/RelationshipController.java
+++ src/main/java/com/example/friendshipneverends/Controller/RelationshipController.java
@@ -1,8 +1,10 @@
 package com.example.friendshipneverends.Controller;
 
 import com.example.friendshipneverends.Entity.Protocol;
+import com.example.friendshipneverends.Entity.Relationship;
 import com.example.friendshipneverends.Service.RelationshipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,21 +19,26 @@ public class RelationshipController {
 
     private static String personalHost = "${IP_ADDRESS}";
 
-    @PostMapping("/relationship")
-    public void relationship(@RequestBody String body){
+    @PostMapping( "/relationship")
+    public ResponseEntity<String> postGreetingRoot(@RequestBody String body){
+       // System.out.println("Root request:" + );
+
+
         Protocol protocol = new Protocol(body);
 
         //Check if the destinationHost is the same as ours
         if(protocol.getDestinationHost().equals(personalHost)){
             //If that's the case, create a request in the db
             relationshipService.manipulateRelationship(protocol);
-            //Send a response back to the source user
+            //Send a response back to the source user'
+            return ResponseEntity.ok("Response");
         }
         else {
-            //Otherwise send a request to the destinationHost with the same protocol
+            //Otherwise, send a request to the destinationHost with the same protocol
 
             //When response is received, create a copy on own database
             relationshipService.manipulateRelationship(protocol);
         }
+        return ResponseEntity.ok("Completed");
     }
 }
